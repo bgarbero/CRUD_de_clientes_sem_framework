449d34244e7808208b2c0ccc87cb5dee2b78acd4
bgarbero <bruno_garbero@hotmail.com>
2023-08-29 16:06:29 -0300

    navegação entre páginas

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ let senha = document.getElementById('senha');
 let btnEntrar = document.getElementById('btn-entrar');
 
 //captura do evento clicar botão
-btnEntrar.addEventListener('click', () =>{
+btnEntrar.addEventListener('click', () => {
     //pegar o email digitado
     let userEmail = email.value;
 
@@ -12,7 +12,7 @@ btnEntrar.addEventListener('click', () =>{
     let userSenha = senha.value;
 
     //validar se o email e senha estão corretos
-    if (!userEmail || !userSenha){
+    if (!userEmail || !userSenha) {
         //caso esteja incorreto, mandar mensagem de usuário e senha inválida
         alert('O campo de e-mail e senha são obrigatórios');
         return;
@@ -23,6 +23,35 @@ btnEntrar.addEventListener('click', () =>{
 })
 
 //criando a função autenticar no backend
-function autenticar(email, senha){
-    
+function autenticar(email, senha) {
+    const urlBase = `http://localhost:3400`;
+
+    fetch(`${urlBase}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, senha })
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (!!response.mensagem) {
+                alert(response.mensagem);
+                return;
+            } else {
+                alert('Usuário autenticado com sucesso!');
+                salvarToken(response.token);
+                salvarUsuario(response.usuario);
+
+                window.open('crud.html', '_self');
+            }
+        })
+}
+
+function salvarToken(token) {
+    localStorage.setItem('token', token)
+}
+
+function salvarUsuario(usuario) {
+    localStorage.setItem('usuario', JSON.stringify(usuario))
 }
