f0731dff7100f3eaa3d7e5ddfb6759bafd708c55
bgarbero <bruno_garbero@hotmail.com>
2023-08-30 14:41:16 -0300

    modal funcionando

--- js/crud.js
+++ js/crud.js
@@ -1,4 +1,14 @@
 const URL = 'http://localhost:3400/clientes';
 
 let modoEdicao = false;
-let btnAdicionar = documet.get
+let btnAdicionar = document.getElementById('btn-adicionar');
+let tabelaCliente = document.querySelector('table>tbody');//observar essa variável
+let modalCliente = new bootstrap.Modal(document.getElementById('modal-cliente'), {});
+let tituloModal = document.querySelector('h4.modal-title');
+
+btnAdicionar.addEventListener('click', () => {
+    modoEdicao = false;
+    tituloModal.textContent = 'Adicionar Cliente';
+    modalCliente.show();
+});
+
