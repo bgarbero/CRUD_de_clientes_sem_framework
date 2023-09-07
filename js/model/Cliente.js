0bf01b45bbe52f1dab38c43ae121ba57edcd1353
bgarbero <bruno_garbero@hotmail.com>
2023-09-07 07:44:08 -0300

    correção do bug do CPF. Motivo: erro de nomenclatura no arquivo cliente.js

--- js/model/Cliente.js
+++ js/model/Cliente.js
@@ -5,7 +5,7 @@ class Cliente {
         obj = obj || {};
         this.id = obj.id;
         this.nome = obj.nome;
-        this.cpfOuCnpj = obj.cpf;
+        this.cpfOuCnpj = obj.cpfOuCnpj;
         this.email = obj.email;
         this.telefone = obj.telefone;
         this.dataCadastro = obj.dataCadastro;
